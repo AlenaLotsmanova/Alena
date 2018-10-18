867d7032ba16e904c8a6e76645c1843a61d6deaf
AlenaLotsmanova <alena_locmanova@mail.ru>
2018-10-18 09:03:27 +0500

    добавила тесты
    Добавила тесты 6 штук.
    1-3 - определяем истина или ложь введенные слова.
    4-5 -  проверяем количество введенных слов.
    6- проверяем слова если больше двух слов

---
+++ test1/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TZ;
+
+
+namespace test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] stroka = { "bukv", "vkub" };
+            string expected = "истина";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string[] stroka = { "вфывв", "фывфывфыцц" };
+            string expected = "ложь";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string[] stroka = { "Сфывв", "фывфывфыццс" };
+            string expected = "истина";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string[] stroka = { "Сфывв" };
+            string expected = "Вы ошиблись";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string[] stroka = {  };
+            string expected = "Вы ошиблись";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string[] stroka = {"gkjhijgfh", "hngfhjmgknmg" , "lnlknmknm" };
+            string expected = "истина";
+
+            // получение значения с помощью тестируемого метода
+            Sum g = new Sum();
+            string rezult = g.Nana(stroka);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+    }
+}
--- алена/Program.cs
+++ алена/Program.cs
@@ -14,25 +14,54 @@ namespace TZ
             while (true)
             {
                 Console.WriteLine("Введите два слова");
-                string str = Console.ReadLine().ToUpper(); //преобразование всех букв в большой регистр
+                string str = Console.ReadLine(); //преобразование всех букв в большой регистр
                 split = str.Split(' ');
                 if (split.Length == 2)
                 {
                     break;
                 }
+                Sum d = new Sum();
+                str = d.Nana(split);
+                Console.WriteLine(str);
             }
-            string t = split[1];
-            string s = Convert.ToString(split[0][0]);
-            string j = Convert.ToString(split[1] [t.Length - 1]); 
-
-                if (s == j)
-                {Console.WriteLine("истина");}
-                else
-                { Console.WriteLine("ложь");}
-                { Console.ReadLine();}
-            }
+            
+             Console.ReadLine();
+            
         }
     }
+    public class Sum
+    {
+        public string Nana(string[] split)
+        {
+            try
+            {
+                string t = split[1];
+                string s = Convert.ToString(split[0].ToUpper()[0]);
+                string j = Convert.ToString(split[1].ToUpper()[t.Length - 1]);
+                if (s == j)
+                {
+                    return "истина";
+                }
+                else
+                {
+                    return "ложь";
+                }
+            }
+            catch (Exception)
+            {
+                return "Вы ошиблись";
+            }
+            
+
+            
+            
+        }
+    }
+}
+       
+            
+        
+   
 
             
        
