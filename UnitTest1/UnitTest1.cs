4634165714c8c56490806b2ee4f9ad5e017ad4b8
AlenaLotsmanova <alena_locmanova@mail.ru>
2018-10-27 10:40:23 +0500

    изменила код в тестах

---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TZ;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Буквы_не_совпадают()
+        {
+            string[] stroka = { "вфывв", "фывфывфыцц" };
+            bool expected = false;
+            // получение значения с помощью тестируемого метода
+            Program f = new Program();
+            bool rezult = f.razdelenie(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void Буквы_совпадают()
+        {
+            string[] stroka = { "Сфывв", "фывфывфыццс" };
+            bool expected = true;
+            // получение значения с помощью тестируемого метода
+            Program f = new Program();
+            bool rezult = f.razdelenie(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void Ввод_одного_слова()
+        {
+            string[] stroka = { "Сфывв" };
+            bool expected = false;
+            // получение значения с помощью тестируемого метода
+            Program f = new Program();
+            bool rezult = f.razdelenie(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+        [TestMethod]
+        public void Не_ввели_слова()
+        {
+            string[] stroka = { };
+            bool expected = false;
+            // получение значения с помощью тестируемого метода
+            Program f = new Program();
+            bool rezult = f.razdelenie(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+            
+
+        }
+        [TestMethod]
+        public void Ввели_более_двух_слов_и_буквы_совпадают()
+        {
+            string[] stroka = { "gkjhijgfh", "hngfhjmgknmg", "lnlknmknm" };
+            bool expected = true;
+            // получение значения с помощью тестируемого метода
+            Program f = new Program();
+            bool rezult = f.razdelenie(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+    }
+}
--- алена/Program.cs
+++ алена/Program.cs
@@ -8,35 +8,9 @@ namespace TZ
 {
     public class Program 
     {
-        static void Main(string[] args)
+        public bool razdelenie(string[] split)
         {
-            string s = "0";
-            string j = "0";
-            int count = 0;
-            string[] split = null;
-            while (true)
-            {
-                Console.WriteLine("Введите два слова");
-                string str = Console.ReadLine(); 
 
-                split = str.Split(' ');
-                if (split.Length == 2)
-                {
-                    break;
-                }
-            }
-
-            foreach (var t in split)
-            {
-                if (count == 0)
-                    s = Convert.ToString(t[0]);
-                if (count == 1)
-                {
-                    int u = t.Length;
-                    j = Convert.ToString(t[u - 1]);
-                }
-                count++;
-            }
             try
             {
                 string t = split[1];
@@ -44,23 +18,49 @@ namespace TZ
                 string j = Convert.ToString(split[1].ToUpper()[t.Length - 1]);
                 if (s == j)
                 {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string[] split = null;
+            while (true)
+            {
+                Console.WriteLine("Введите два слова");
+                string str = Console.ReadLine();
+
+                split = str.Split(' ');
+                if (split.Length == 2)
+                {
+                    break;
+                }
+                Program f = new Program();
+                bool res = f.razdelenie(split);
+                if (res == true)
+                {
                     Console.WriteLine("истина");
                 }
                 else
                 {
                     Console.WriteLine("ложь");
                 }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Вы ошиблись");
-            }
-            {
                 Console.ReadLine();
             }
         }
+        
     }
- }
+}
+
 
 
 
